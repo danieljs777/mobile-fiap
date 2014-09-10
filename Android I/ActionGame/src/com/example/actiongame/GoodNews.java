6230f07a3d5c705f493fbcb441b793e8dd370eda
Daniel Jordão <daniel.js@gmail.com>
2014-09-10 19:03:16 -0300

    Novo Projeto ActionGame - Interface incompleta

---
+++ Android I/ActionGame/src/com/example/actiongame/GoodNews.java
@@ -0,0 +1,28 @@
+package com.example.actiongame;
+
+public class GoodNews implements Runnable
+{
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			Random randomGenerator = new Random( ) ;
+			int rnd = 0 ;
+			int threadNo = 0 ;
+
+			while (i <= myMaxTics)
+			{
+
+				Thread.sleep( 1000 ) ;
+				rnd = randomGenerator.nextInt( 4 ) ;
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
